70b4e5373f05199d490105c5d078fcd091f6f262
hyeonjeong9911 <knk990101@naver.com>
2024-11-27 15:05:56 +0900

    refactor: pet 관련 기능 프로젝트 분리

--- src/main/java/com/example/insurance/pet/repository/BreedCodeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.insurance.pet.repository;
-
-import com.example.insurance.pet.entity.code.BreedCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BreedCodeRepository extends JpaRepository<BreedCode, String> {
-
-}
--- src/main/java/com/example/insurance/pet/repository/PetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.insurance.pet.repository;
-
-import com.example.insurance.pet.entity.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetRepository extends JpaRepository<Pet, Long> {
-
-}
